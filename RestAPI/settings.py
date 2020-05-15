d2290b6bb4b5653296f0a46e491833eb045ff736
Syed Kashif Ali <mehmoodkashif09@gmail.com>
2020-05-16 02:24:20 +0500

    Update settings.py

--- RestAPI/settings.py
+++ RestAPI/settings.py
@@ -134,7 +134,7 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     #Allow all host headers
-    ALLOWED_HOSTS = ['RestAPI.herokuapp.com']
+    ALLOWED_HOSTS = ['restapi-serializer.herokuapp.com']
     DEBUG = True
 
     #Static asset configuration
